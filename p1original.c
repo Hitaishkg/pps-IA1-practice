a6d7defe953b7634775282438f23ca75bc5645fe
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-02-04 03:53:48 +0000

    ..

--- p1original.c
+++ p1original.c
@@ -1,2 +1,17 @@
-
+#include<stdio.h>
+void input(int *a, int *b){
+    printf("Enter 2 numbers to be added\n");
+    scanf("%d %d",a,b);
+}
+void add(int a , int b, int *sum){
+    *sum=a+b;
+}
+void output(int a,int b, int sum){
+    printf("sum of %d + %d is %d",a,b,sum);
+}
+int main(){
+    int a,b,sum;
+    input(&a,&b);
+    add(a,b,&sum);
+    output(a,b,sum);
 }
